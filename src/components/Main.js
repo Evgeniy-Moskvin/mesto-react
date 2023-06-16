12c2640431f8638b5141f2cddf1308ef42ed5fd1
Evgeniy-Moskvin <moskwin.evgeny@gmail.com>
2023-06-16 12:26:19 +0300

    feat: подключить api для данных профиля

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,35 @@
 import React from 'react';
+import { api } from '../utils/api';
 
 const Main = () => {
 
+    const [userName, setUserName] = React.useState('');
+    const [userDescription, setUserDescription] = React.useState('');
+    const [userAvatar, setUserAvatar] = React.useState('');
+
+    React.useEffect(() => {
+        api.getUserInfo().then(data => {
+            setUserName(data.name);
+            setUserDescription(data.about);
+            setUserAvatar(data.avatar);
+        })
+    }, []);
 
     return (
         <main>
             <section className="profile container">
                 <div className="profile__avatar-wrap">
-                    <img src="#" alt="Аватар пользователя" className="profile__avatar"/>
+                    <img src={userAvatar} alt="Аватар пользователя" className="profile__avatar"/>
                 </div>
 
                 <div className="profile__info">
                     <div className="profile__name-wrap">
-                        <h1 className="profile__name">Жак-Ив Кусто</h1>
+                        <h1 className="profile__name">{userName}</h1>
                         <button type="button" className="button-edit profile__button-edit opacity-effect"
                                 aria-label="Редактировать профиль"></button>
                     </div>
 
-                    <p className="profile__job">Исследователь океана</p>
+                    <p className="profile__job">{userDescription}</p>
                 </div>
 
                 <button type="button" className="button-add profile__button-add opacity-effect"
